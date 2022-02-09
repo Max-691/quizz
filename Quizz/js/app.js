1009b95d3790edfccd6f375562ba6415911d4ddb
NbdyMflt <nbdy.nflt.sna@gmail.com>
2022-02-09 11:50:46 +0100

    Javascript edited but new problems. To do...

--- Quizz/js/app.js
+++ Quizz/js/app.js
@@ -22,15 +22,32 @@ let article = document.getElementsByClassName("article");
 
 
 button.addEventListener('click', function() {
+    let goodAnswer = 0;
+    let wrongAnswer = 0;
+    let allAnswers = goodAnswer + wrongAnswer;
     for (let i = 0; i < goodAnswer.length; i++) {
         if(goodAnswer[i].checked){
             article[i].style.background = "green";
+            goodAnswer++;
         }
         else{
             article[i].style.background = "red";
+            wrongAnswer++;
         }
     }
+    //Display how much wrong answer rest to done when button is clicked
+    const wrong_id = document.getElementById("emoji");
+    wrong_id.textContent = "👀 Il reste quelques erreurs 😭";
+
+    //Display the sentence when button is clicked
+    const edit_p = document.getElementById("edit_p");
+    edit_p.textContent = "Retentez une autre réponse dans les cases rouges, puis re-validez !"; 
+
+    const score = document.getElementById("score");
+    score = score.textContent = wrongAnswer + "/" + allAnswers;
+
 });
+
 
 
 
